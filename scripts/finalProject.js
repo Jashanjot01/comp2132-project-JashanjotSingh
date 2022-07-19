b0d245cdf84e146606802cdb4f9ddedf443f2b00
Jashanjot Singh <jashanjotsingh@my.bcit.ca>
2022-07-19 14:32:09 -0700

    Added jquery and other stuff

---
+++ scripts/finalProject.js
@@ -0,0 +1,6 @@
+let j = Math.floor(Math.random()*6);
+
+document.getElementById("playerYou").innerHTML = `<h2><img src="images/product-images/dice-six-faces-${j+1}.png" alt=""></h2>`;
+
+// $("playerOpponent").html(`<h2><img src="images/product-images/dice-six-faces-${j+1}.png" alt=""></h2>`);
+// $("playerOpponent").css('background-color', 'red');
