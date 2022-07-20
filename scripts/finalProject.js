e49d9ecf20bb9f1f89fdc8a73d11ecadcf3546b2
Jashanjot Singh <jashanjotsingh@my.bcit.ca>
2022-07-19 17:36:34 -0700

    Play button working

--- scripts/finalProject.js
+++ scripts/finalProject.js
@@ -1,80 +1,117 @@
-const playerYou = document.getElementById("playerYou");  
+const playerYou = document.getElementById("playerYou");
 const playerOpponent = document.getElementById("playerOpponent");
+const yourFinalScore = document.getElementById("yourFinalScore");
+const opponentFinalScore = document.getElementById("opponentFinalScore");
 
 const playButton = document.getElementById("play");
 const resetButton = document.getElementById("reset");
-let i,j;      
+let i, j, score;
+let yourScore = 0;
+let opponentScore = 0;
+let count = 0;
 // let j = Math.floor(Math.random()*6);
 
 // document.getElementById("playerYou").innerHTML = `<h2><img src="images/product-images/dice-six-faces-${j+1}.png" alt=""></h2>`;
 
 // $("playerOpponent").html(`<h2><img src="images/product-images/dice-six-faces-${j+1}.png" alt=""></h2>`);
-// $("playerOpponent").css('background-color', 'red');
+$("playerOpponent").css('background-color', 'red');
 
-function runDice(){
-    j = Math.floor(Math.random()*6);
-    return `<img src="images/product-images/dice-six-faces-${j+1}.png" alt="">`;
+$("play").click(function(){
+    alert('Hi');
+})
+
+function resetEverything(){
+    playerYou.innerHTML = '';
+    playerOpponent.innerHTML = '';
+    yourFinalScore = '';
+    opponentFinalScore = '';
+    count = 0;
+    score = 0;
+    yourScore = 0;
+    opponentScore = 0;
 }
 
-function runSecondDice(){
-    i = Math.floor(Math.random()*6);
-    return `<img src="images/product-images/dice-six-faces-${i+1}.png" alt="">`;
+function runDice() {
+    j = Math.floor(Math.random() * 6);
+    return `<img src="images/product-images/dice-six-faces-${j + 1}.png" alt="">`;
 }
 
-// function scoring(){
-//     if (i === j && i != 0 && j != 0){
-//         return "<h2>Wow</h2>";
-//     } else if (i == 0 || j == 0 ){
-//         return "<h2>Bad</h2>";
-//     } else {
-//         return "<h2>ok<h2>";
-//     }
-// }
+function runSecondDice() {
+    i = Math.floor(Math.random() * 6);
+    return `<img src="images/product-images/dice-six-faces-${i + 1}.png" alt="">`;
+}
 
-class Player{
-    constructor (name){
+class Player {
+
+    constructor(name) {
         this.name = name;
-        this.dice=[];
     }
 
-    numberOfDice(aDice){
-        this.dice.push(aDice);
-    }
-
-    scoring(){
-        if (i === j && i != 0 && j != 0){
-            return "<h2>Wow</h2>";
-        } else if (i == 0 || j == 0 ){
-            return "<h2>Bad</h2>";
-        } else {
-            return "<h2>ok<h2>";
-        }
-    }
-
-    describeSelf(){
+    describeSelf() {
         let string = `<h2>${this.name}<h2>`;
-        this.dice.forEach(function(oneDice){
-            string += `<li>${oneDice}</li>`;
-        })
+        string += `<ul>`;
+        string += `<li>${runDice()}</li>`;
+        string += `<li>${runSecondDice()}</li>`;
+        string += `</ul>`;
+        if (i === j && i != 0 && j != 0) {
+            score = (i+j)*2;
+        } else if (i == 0 || j == 0) {
+            score = 0;
+        } else {
+            score = i+j;
+        }
+        string += `<h2>Score: ${score}</h2>`;
         return string;
     }
 }
 
-
 const player01 = new Player("john");
-
-player01.numberOfDice(runDice());
-player01.numberOfDice(runSecondDice());
-playerYou.innerHTML = player01.describeSelf();
-playerYou.innerHTML += player01.scoring();
-
-
 const player02 = new Player("jill");
 
-player02.numberOfDice(runDice());
-player02.numberOfDice(runSecondDice());
-playerOpponent.innerHTML = player02.describeSelf();
-playerOpponent.innerHTML += player02.scoring();
+playButton.addEventListener('click', function () {
+    count ++;
+    if (count <= 3 ){
+    playerYou.innerHTML = player01.describeSelf();
+    yourScore = yourScore + score;
+    yourFinalScore.innerHTML = `<h2>Total Score: ${yourScore}</h2>`;
+
+    playerOpponent.innerHTML = player02.describeSelf();
+    opponentScore = opponentScore + score;
+    opponentFinalScore.innerHTML = `<h2>Total Score: ${opponentScore}</h2>`;
+    } 
+    else {
+        this.removeEventListener;
+    }
+    
+    if (count == 3)
+    {
+        if (yourScore > opponentScore){
+        yourFinalScore.innerHTML += "You won";
+        }
+        else if (yourScore == opponentScore){
+        yourFinalScore.innerHTML += "Tied";
+        opponentFinalScore.innerHTML += "Tied";
+        }
+        else{
+            opponentFinalScore.innerHTML += "You won";
+        }
+
+    }
+})
+
+function resetEverything(){
+    // playerYou.innerHTML = '';
+    // playerOpponent.innerHTML = '';
+    // yourFinalScore = '';
+    // opponentFinalScore = '';
+    count = 0;
+    score = 0;
+    yourScore = 0;
+    opponentScore = 0;
+}
+
+resetButton.addEventListener('click', resetEverything);
+
 
 
 
